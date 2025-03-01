f7503bc3327dada5fd0a867ba3d4e1241735e1ea
ADDikt88 <kyletsang8@gmail.com>
2025-03-01 01:54:49 -0500

    Table is sortable by column header
    Name, category, quantity, date last used can now be sorted
    alphabetically or numerically

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -6,6 +6,9 @@ function ItemList({ items }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  //Sort mode
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: "asc" });
+
   const loadItems = async () => {
     setLoading(true);
     setError(null);
@@ -40,18 +43,98 @@ function ItemList({ items }) {
     //loadItems();
   };
 
+  const handleSort = (keyChoice) => {
+    let directionChoice = "asc";
+    if (sortConfig.key === keyChoice && sortConfig.direction === "asc") {
+      console.log("switching to desc");
+      directionChoice = "desc";
+    }
+    setSortConfig({ key: keyChoice, direction: directionChoice });
+
+    setDisplayItems(
+      displayItems.sort((a, b) => {
+        //set conditions
+
+        if (keyChoice === "quantity") {
+          if (sortConfig.key !== keyChoice) {
+            console.log("new choice ", keyChoice);
+            return a[keyChoice] > b[keyChoice] ? 1 : -1;
+          }
+          if (directionChoice === "asc") {
+            console.log("asc ");
+            return a[sortConfig.key] > b[sortConfig.key] ? 1 : -1;
+          }
+          if (directionChoice === "desc") {
+            console.log("desc ");
+            return a[sortConfig.key] < b[sortConfig.key] ? 1 : -1;
+          }
+        } else {
+          if (sortConfig.key !== keyChoice) {
+            console.log("new choice ", keyChoice);
+            return a[keyChoice].toLowerCase() > b[keyChoice].toLowerCase()
+              ? 1
+              : -1;
+          }
+          if (directionChoice === "asc") {
+            console.log("asc ");
+            return a[sortConfig.key].toLowerCase() >
+              b[sortConfig.key].toLowerCase()
+              ? 1
+              : -1;
+          }
+          if (directionChoice === "desc") {
+            console.log("desc ");
+            return a[sortConfig.key].toLowerCase() <
+              b[sortConfig.key].toLowerCase()
+              ? 1
+              : -1;
+          }
+        }
+      })
+    );
+  };
+
   return (
     <div>
       <h1>Current Inventory</h1>
       <table border="1" cellPadding="10">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Category</th>
+            <th onClick={() => handleSort("name")}>
+              Name{" "}
+              {sortConfig.key === "name"
+                ? sortConfig.direction === "asc"
+                  ? "⬆"
+                  : "⬇"
+                : ""}
+            </th>
+            <th onClick={() => handleSort("category")}>
+              Category{" "}
+              {sortConfig.key === "category"
+                ? sortConfig.direction === "asc"
+                  ? "⬆"
+                  : "⬇"
+                : ""}
+            </th>
             <th>Age Range</th>
-            <th>Quantity</th>
-            <th>Date Last Used</th>
+            <th onClick={() => handleSort("quantity")}>
+              Quantity{" "}
+              {sortConfig.key === "quantity"
+                ? sortConfig.direction === "asc"
+                  ? "⬆"
+                  : "⬇"
+                : ""}
+            </th>
+            <th onClick={() => handleSort("last_used")}>
+              Date Last Used{" "}
+              {sortConfig.key === "last_used"
+                ? sortConfig.direction === "asc"
+                  ? "⬆"
+                  : "⬇"
+                : ""}
+            </th>
             <th>Description</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -96,14 +179,14 @@ function ItemList({ items }) {
                 <div className="edit-del-cell">
                   <button
                     className="edit-btn"
-                    key={item.id}
-                    onClick={() => handleDelete(item.id)}
+                    onClick={(e) =>
+                      e.preventDefault()
+                    } /*Impelement edit mode soon... */
                   >
                     Edit
                   </button>
                   <button
                     className="del-btn"
-                    key={item.id}
                     onClick={() => handleDelete(item.id)}
                   >
                     Delete
