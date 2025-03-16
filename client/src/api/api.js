7f2583fab548bae93b2092ac210e87a86e15d2f4
ADDikt88 <kyletsang8@gmail.com>
2025-03-16 13:05:35 -0400

    New build

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://inventory-app-xopm.onrender.com/";
+const API_BASE_URL = "https://inventory-app-xopm.onrender.com";
 //const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 //const API_BASE_URL = "http://localhost:3000";
 
